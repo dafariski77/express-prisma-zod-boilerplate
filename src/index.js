ff8ff1f16428c55f9b87ec13990282057bfe5365
dafariski77 <rskdafa@gmail.com>
2024-01-21 15:12:40 +0700

    chore: forgot to add server wkwkwk :v

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import http from "http";
+import errorHandlerMiddleware from "./middlewares/handleError.js";
+import cors from "cors";
+import routes from './routes/index.js'
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/v1", routes);
+
+app.get("/", (req, res) => {
+  return res.json("Hello world");
+});
+
+app.use(errorHandlerMiddleware);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+server.on("listening", () => {
+  console.log(`Server running on port ${port}`);
+});
