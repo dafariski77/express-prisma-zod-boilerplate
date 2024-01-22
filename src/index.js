b75251ccc1fb2ba569c69e08fe41394757f6cef8
dafariski77 <rskdafa@gmail.com>
2024-01-22 21:57:49 +0700

    chore: add several package for server

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import morgan from "morgan";
 import http from "http";
 import errorHandlerMiddleware from "./middlewares/handleError.js";
 import cors from "cors";
+import helmet from "helmet";
 import routes from "./routes/index.js";
 import handleNotFoundRoute from "./middlewares/notFoundRoute.js";
+import compression from "compression";
+import xss from "xss";
 
 dotenv.config();
 
@@ -14,8 +17,12 @@ const port = process.env.PORT || 9000;
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(xss());
 
 app.use("/api/v1", routes);
 
