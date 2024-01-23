53bcd19c7892eae630413c623a9ee4ee9e017aa8
dafariski77 <rskdafa@gmail.com>
2024-01-24 00:24:04 +0700

    feat(auth): add schema validation for verify

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -28,7 +28,23 @@ const login = z.object({
   }),
 });
 
+const verify = z.object({
+  body: z.object({
+    otp: z
+      .number({ required_error: "OTP perlu diisi!" })
+      .int()
+      .gte(100000)
+      .lte(999999),
+    email: z
+      .string({
+        required_error: "Email perlu diisi!",
+      })
+      .email("Email tidak valid!"),
+  }),
+});
+
 export default {
   register,
   login,
+  verify
 };
