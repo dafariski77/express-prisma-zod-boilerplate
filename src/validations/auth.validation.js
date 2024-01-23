360ca4f9bbe7f952d150b454f772a8291524347c
dafariski77 <rskdafa@gmail.com>
2024-01-23 23:58:19 +0700

    feat(auth): add schema validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,24 +1,34 @@
+import { z } from "zod";
+
 const register = z.object({
-  body: z
-    .object({
-      fullName: z.string({
-        required_error: "Fullname required!",
-      }),
-      email: z
-        .string({
-          required_error: "Email is required!",
-        })
-        .email("Email not valid!"),
-      password: z.string({ required_error: "Password is required!" }),
-      passwordConfirmation: z.string({
-        required_error: "Password confirmation is required!",
-      }),
-    })
-    .required({
-      email: true,
+  body: z.object({
+    fullname: z.string({
+      required_error: "Fullname perlu diisi!",
     }),
+    email: z
+      .string({
+        required_error: "Email perlu diisi!",
+      })
+      .email("Email tidak valid!"),
+    password: z.string({ required_error: "Password perlu diisi!" }),
+    passwordConfirmation: z.string({
+      required_error: "Password confirmation perlu diisi!",
+    }),
+  }),
+});
+
+const login = z.object({
+  body: z.object({
+    email: z
+      .string({
+        required_error: "Email perlu diisi!",
+      })
+      .email("Email tidak valid!"),
+    password: z.string({ required_error: "Password perlu diisi!" }),
+  }),
 });
 
 export default {
   register,
+  login,
 };
